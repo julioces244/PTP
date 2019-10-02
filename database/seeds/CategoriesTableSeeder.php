e526395a91077682ca2231f92d7d6006037b10f1
Julio César <julio.apaza@tecsup.edu.pe>
2019-10-02 10:54:03 -0500

    Commit 02/10 v1.03

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('categories')->insert([
+            'nombre' => 'Pisco Aprendizaje',
+            'abreviatura' => 'PA',
+        ]);
+
+        DB::table('categories')->insert([
+            'nombre' => 'Pisco Digital',
+            'abreviatura' => 'PD',
+        ]);
+
+        DB::table('categories')->insert([
+            'nombre' => 'Pisco Tecnología',
+            'abreviatura' => 'PT',
+        ]);
+    }
+}
---
+++ database/seeds/TypearchivesTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class TypearchivesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('typearchives')->insert([
+            'nombre' => 'Informe Semanal',
+            'abreviatura' => 'IS',
+        ]);
+
+        DB::table('typearchives')->insert([
+            'nombre' => 'Informe Bimensual',
+            'abreviatura' => 'IB',
+        ]);
+
+        DB::table('typearchives')->insert([
+            'nombre' => 'Otros documentos',
+            'abreviatura' => 'OD',
+        ]);
+    }
+}
