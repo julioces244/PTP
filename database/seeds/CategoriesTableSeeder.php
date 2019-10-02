94f301be7de926791420cfd68fbb9a28dc23769b
Julio César <julio.apaza@tecsup.edu.pe>
2019-10-02 10:57:56 -0500

    Commit 02/10 v1.03

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class CategoryTableSeeder extends Seeder
+class CategoriesTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
